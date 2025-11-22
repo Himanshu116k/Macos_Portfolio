fb4cef7ffea27cf3aa75e4f260a01984189a61c5
Himanshu116k <himanshu1991patna@gmail.com>
2025-11-22 15:47:32 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "../constants/index.js";
+
+const Navbar = () => {
+  const data = [
+    { id: 1, nam: "Portfolio" },
+    { id: 2, nam: "Contact" },
+    { id: 3, nam: "Projects" },
+  ];
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="">Himanshu Portfolio</p>
+        <ul>
+          {navLinks.map((item) => (
+            <li key={item.id}>
+              <p>{item.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+            {navIcons.map(({id, img}) => (
+                <li key={id}>
+                    <img src={img} className="icon-hover"  alt={`icon-${id}`} />
+                </li>
+            ))}
+        </ul>
+        <time>
+            {dayjs().format("ddd MMM D h:mm A")}
+        </time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
