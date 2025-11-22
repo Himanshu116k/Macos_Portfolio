d3be0c9708cf6ffe8ac670ddf4af7ff33597e203
Himanshu116k <himanshu1991patna@gmail.com>
2025-11-22 20:50:23 +0530

    adding higher order fuction

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from '#components/Navbar.jsx'
 import Welcome from './components/Welcome'
 import Dock from './components/Dock'
+import { Draggable } from 'gsap/Draggable'
+import gsap from 'gsap'
+import Terminal from "./windows/Terminal.jsx"
+gsap.registerPlugin(Draggable)
 
 const App = () => {
   return (
@@ -9,6 +13,8 @@ const App = () => {
     <Navbar/>
     <Welcome/>
     <Dock/>
+
+    <Terminal/>
    </main>
   )
 }
